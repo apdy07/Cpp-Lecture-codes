f6c4a230462b1684866d89dbc341c8514be07059
apdy07 <119981586+apdy07@users.noreply.github.com>
2023-02-28 06:27:14 +0530

    Update Linear Search.cpp

--- Lec 6/Linear Search.cpp
+++ Lec 6/Linear Search.cpp
@@ -3,18 +3,19 @@ using namespace std;
 int main(){
     int arr[] = {10,20,30,40,50};
     int n = sizeof(arr)/sizeof(arr[0]);
-    int key;
+    int key,i;
     cin>>key;
-    for(int i=0;i<n;i++){
+    for(i=0;i<n;i++){
         cin>>arr[i];
     }
-    for(int i=0;i<=n;i++){
+    for(i=0;i<n;i++){
         if(arr[i] == key){
             cout<<i+1;
             break;
         }
+    }
         if(i==n){
         cout<<"-1";
         }
-    }
+    
 }
